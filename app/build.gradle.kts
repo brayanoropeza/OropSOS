32606625ea9b535b973063fe7262e1334e361606
Brayan Jesus Oropeza Acuna <brayanoropeza@gmail.com>
2026-08-25 12:04:55 -0600

    OropSOS: Agregar firma digital permanente keystore.jks para permitir actualizar el APK sin desinstalar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,22 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = "oropsos_password"
+            keyAlias = "oropsos_key"
+            keyPassword = "oropsos_password"
+        }
+    }
+
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("release")
+        }
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
